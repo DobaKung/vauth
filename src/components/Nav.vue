7ddac8ca50e8801b84cc8ea9978b37517a8cf641
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-13 22:41:55 +0700

    feat: style nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,32 @@
 <template>
-  <nav id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/register">Register</router-link>
+  <nav id="nav" class="text-right font-bold py-6 px-3">
+    <router-link
+      v-for="link in links"
+      :to="link.to"
+      class="mx-3 text-slate-400 hover:text-accent"
+      >{{ link.text }}</router-link
+    >
   </nav>
 </template>
+
+<style scoped>
+nav a.router-link-active {
+  /*  slate-900 */
+  color: #0f172a;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: { title: String },
+  data: () => ({
+    links: [
+      { to: "/", text: "Home" },
+      { to: "/register", text: "Register" },
+      { to: "/log-in", text: "Log in" },
+    ],
+  }),
+});
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     name: "Register",
     component: () => import("../views/Register.vue"),
   },
+  {
+    path: "/log-in",
+    name: "Log in",
+    component: Home, // temporary
+  },
 ];
 
 const router = createRouter({
