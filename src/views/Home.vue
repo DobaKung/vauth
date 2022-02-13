fbc356992c2ccd95cb8b11aeccfdc1b33bc0093c
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-13 22:59:13 +0700

    feat: add cta to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,18 @@
   <div class="container max-w-screen-xl mx-auto px-4 py-4 text-center">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <h1 class="text-6xl font-bold mt-[160px] mb-3">Voice Matching System</h1>
-    <h2 class="text-3xl font-bold mb-10">Demonstration</h2>
+    <h2 class="text-3xl font-bold">Demonstration</h2>
+    <router-link
+      to="/register"
+      class="inline-block mt-8 px-6 py-3 bg-primary hover:bg-accent text-xl text-white rounded-full shadow-2xl"
+    >
+      Register voice
+    </router-link>
     <img
       src="../assets/211771_mic_icon.svg"
       alt="microphone icon"
       width="180"
-      class="mx-auto mb-12"
+      class="mx-auto mt-12 mb-12"
     />
     <p class="text-slate-500 mx-auto max-w-lg">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur cum ut
