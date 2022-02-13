6704402ef7e05199ab88a611006325b02f31b3c1
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-13 22:41:11 +0700

    refactor: set default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0f172a", // slate-900
+        accent: "#16a34a", // green-600
+      },
+    },
   },
   plugins: [],
 };
