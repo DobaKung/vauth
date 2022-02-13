20866e0b7941a16ad7e8471badab4f2da008466b
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-13 22:41:33 +0700

    feat: add mic icon to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="container max-w-screen-xl mx-auto px-4 py-4">
+  <div class="container max-w-screen-xl mx-auto px-4 py-4 text-center">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <h1 class="text-6xl font-bold text-slate-900 mt-[160px] mb-3">
-      Voice Matching System
-    </h1>
-    <h2 class="text-3xl font-bold text-slate-900 mb-10">Demonstration</h2>
-    <p class="text-slate-500 max-w-lg">
+    <h1 class="text-6xl font-bold mt-[160px] mb-3">Voice Matching System</h1>
+    <h2 class="text-3xl font-bold mb-10">Demonstration</h2>
+    <img
+      src="../assets/211771_mic_icon.svg"
+      alt="microphone icon"
+      width="180"
+      class="mx-auto mb-12"
+    />
+    <p class="text-slate-500 mx-auto max-w-lg">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur cum ut
       delectus voluptates! Asperiores, nobis. Ipsa dolorum omnis velit eveniet
       iure maiores, minima nihil! Eos enim perspiciatis commodi illo possimus!
