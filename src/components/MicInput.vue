cd0182f88f1fb7bd7ccc338ec57c45f00843d5b6
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-16 17:43:37 +0700

    feat: add event to MicInput component

--- src/components/MicInput.vue
+++ src/components/MicInput.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+defineEmits(["recording-ready"]);
+</script>
+
 <template>
-  <div class="bg-slate-200 py-10 text-center">voice area</div>
+  <div class="bg-slate-200 py-10 text-center">
+    <p>voice area</p>
+    <a
+      @click="passRecording"
+      class="px-2 py-1 bg-slate-500 hover:bg-accent text-white rounded-full"
+    >
+      Emit
+    </a>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  methods: {
+    passRecording() {
+      this.$emit("recording-ready", "replace with recording data");
+    },
+  },
+});
+</script>
