ec15986cd05f1a26537bf94a5202b5714ade67d2
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-17 15:36:43 +0700

    feat: separate start/stop btns in MicInput

--- src/components/MicInput.vue
+++ src/components/MicInput.vue
@@ -1,36 +1,56 @@
 <script setup lang="ts">
+import { defineComponent } from "vue";
 defineEmits(["recording-ready", "processing"]);
 </script>
 
 <template>
   <div class="bg-slate-200 py-10 text-center">
     <p>voice area</p>
-    <a
-      @click="debugToggleProcessing"
-      class="px-2 py-1 bg-slate-500 hover:bg-accent text-white rounded-full"
+    <button
+      :disabled="isRecording || isProcessing"
+      @click="setIsRecording"
+      class="px-2 py-1 mx-1 bg-slate-500 hover:bg-accent disabled:bg-slate-300 text-white rounded-full"
     >
-      Process
-    </a>
+      Record
+    </button>
+    <button
+      :disabled="!isRecording"
+      @click="processRecording"
+      class="px-2 py-1 mx-1 bg-slate-500 hover:bg-accent disabled:bg-slate-300 text-white rounded-full"
+    >
+      Stop
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
 export default defineComponent({
+  data: () => ({
+    isRecording: false,
+    isProcessing: false,
+  }),
   methods: {
-    passRecording() {
-      this.$emit("recording-ready", "replace with recording data");
-    },
     setProcessing() {
+      this.isProcessing = true;
+      this.isRecording = false;
       this.$emit("processing");
     },
-    async debugToggleProcessing() {
+    setIsRecording(e: Event) {
+      e.preventDefault();
+      this.isRecording = true;
+    },
+    setRecordingReady() {
+      this.isProcessing = false;
+      this.$emit("recording-ready", "replace with recording data");
+    },
+    async processRecording(e: Event) {
+      e.preventDefault();
       this.setProcessing();
       const p = new Promise((resolve) => {
         setTimeout(() => resolve(true), 500);
       });
       await p;
-      this.passRecording();
+      this.setRecordingReady();
     },
   },
 });
