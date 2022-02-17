b1f7356ea88da73c8cc9ebf672ee8e9a066abe77
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-17 15:10:10 +0700

    feat(view): get identification result from controller

---
+++ src/components/IdentificationResult.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  studentId: String,
+  username: String,
+});
+</script>
+
+<template>
+  <p>
+    Identified as student ID:
+    <code class="text-accent">
+      {{ studentId }}
+    </code>
+    , username:
+    <code class="text-accent">
+      {{ username }}
+    </code>
+  </p>
+</template>
--- src/components/MicInput.vue
+++ src/components/MicInput.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-defineEmits(["recording-ready"]);
+defineEmits(["recording-ready", "processing"]);
 </script>
 
 <template>
   <div class="bg-slate-200 py-10 text-center">
     <p>voice area</p>
     <a
-      @click="passRecording"
+      @click="debugToggleProcessing"
       class="px-2 py-1 bg-slate-500 hover:bg-accent text-white rounded-full"
     >
-      Emit
+      Process
     </a>
   </div>
 </template>
@@ -20,6 +20,17 @@ export default defineComponent({
   methods: {
     passRecording() {
       this.$emit("recording-ready", "replace with recording data");
+    },
+    setProcessing() {
+      this.$emit("processing");
+    },
+    async debugToggleProcessing() {
+      this.setProcessing();
+      const p = new Promise((resolve) => {
+        setTimeout(() => resolve(true), 500);
+      });
+      await p;
+      this.passRecording();
     },
   },
 });
